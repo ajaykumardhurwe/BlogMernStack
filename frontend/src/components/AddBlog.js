2b3c91d48b4cb891beb6d91b2aeca98692098087
Ajay Kumar Dhurwe <ajaykumardhurwe049@gmail.com>
2025-06-29 16:48:17 +0530

    frontend env added

--- frontend/src/components/AddBlog.js
+++ frontend/src/components/AddBlog.js
@@ -21,7 +21,7 @@ const AddBlog = () => {
   };
   const sendRequest = async () => {
     const res = await axios
-      .post("http://localhost:5000/api/blog/add", {
+      .post("https://blogmernstack-5d11.onrender.com/api/blog/add", {
         title: inputs.title,
         description: inputs.description,
         image: inputs.imageURL,
